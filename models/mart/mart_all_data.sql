db4437b5ee527d2364aca2979a151dd00d468d3c
Lukas-ing <lukasgaethje@hotmail.de>
2025-07-28 15:30:18 +0200

    mart_all_data

---
+++ models/mart/mart_all_data.sql
@@ -0,0 +1,23 @@
+SELECT o.CUSTOMER_ID,
+c.GENDER  AS customer_gernder,
+c."location" AS customer_location,
+c.TENURE_MONTHS AS customer_tenure_months,
+o.TRANSACTION_ID, 
+o.TRANSACTION_DATE, 
+o.PRODUKT_SKU,
+o.PRODUCT_CATEGORY,
+o.QUANTITY,
+o.AVG_PRICE,
+o.DELIVERY_CHARGES,
+o.COUPON_STATUS,
+d.discount_pct,
+t.gst AS gst_onsale,
+o.TRANSACTION_YEAR,
+o.TRANSACTION_MONTH,
+o.TRANSACTION_MONTH_STR,
+o.TRANSACTION_DAY_OF_MONTH,
+o.TRANSACTION_DAY_OF_WEEK
+FROM {{ref('prep_online_sales')}} o
+LEFT JOIN {{ref('prep_customers')}} c USING (customer_id)
+LEFT JOIN {{ref('prep_tax_amount')}} t USING (PRODUCT_CATEGORY)
+LEFT JOIN {{ref('prep_discount_coupon')}} d ON (o.TRANSACTION_MONTH_STR, o.PRODUCT_CATEGORY)   =  (d."month", d.product_category)
