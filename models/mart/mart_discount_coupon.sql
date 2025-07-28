61645f05776ba8a83d3955483c268bbc94468cb5
Lukas-ing <lukasgaethje@hotmail.de>
2025-07-28 14:27:56 +0200

    fix

--- models/mart/mart_discount_coupon.sql
+++ models/mart/mart_discount_coupon.sql
@@ -1,4 +1,4 @@
 Select d."start_date",d."month",d."end_date",
 d."product_category",
-d."discount_pc"
+d."discount_pct"
 from {{ref('prep_discount_coupon')}} d
--- models/prep/prep_online_sales.sql
+++ models/prep/prep_online_sales.sql
@@ -3,5 +3,5 @@ date_part('year', o."transaction_date") AS transaction_year,
 date_part('month', o."transaction_date" ) AS transaction_month,
 to_char(o."transaction_date",'FMmon') AS transaction_month_str,
 date_part('day', o."transaction_date") AS transaction_day_of_month,
-to_char( o."transaction_date",'FMday') AS transaction_day_of_week,
+to_char( o."transaction_date",'FMday') AS transaction_day_of_week
 FROM {{ref('staging_online_sales')}} o 
