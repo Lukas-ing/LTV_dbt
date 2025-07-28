0c2294f9b5ed3fa77a02a5f4ab67f7d949c6e489
Lukas-ing <lukasgaethje@hotmail.de>
2025-07-28 13:12:20 +0200

    fixes in staging

--- models/staging/staging_customers.sql
+++ models/staging/staging_customers.sql
@@ -2,4 +2,4 @@ SELECT c."CustomerID" AS customer_id,
 c."Gender" AS gender,
 c."Location" AS location,
 c."Tenure_Months" AS tenure_months
-FROM {{source('data', 'customers')}}
+FROM {{source('data', 'customers')}} c
