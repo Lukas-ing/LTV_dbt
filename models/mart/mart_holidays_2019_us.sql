3e2a82d02fd89983859dbb3dd666d53b26ea0bfb
Lukas-ing <lukasgaethje@hotmail.de>
2025-07-29 10:19:27 +0200

    added holiday classification

--- models/mart/mart_holidays_2019_us.sql
+++ models/mart/mart_holidays_2019_us.sql
@@ -1,2 +1,14 @@
-Select *
+SELECT *,
+CASE 
+	WHEN lower(h.type) IN ('christian','hebrew','hinduism','local holiday, christian','national holiday, christian') THEN TRUE 
+	ELSE FALSE 
+END AS religious_holiday,
+CASE 
+	WHEN lower(h.type) IN ('local holiday','local holiday, christian') THEN TRUE 
+	ELSE FALSE 
+END AS local_holiday,
+CASE 
+	WHEN lower(h.type) IN ('national holiday','national holiday, christian') THEN TRUE 
+	ELSE FALSE 
+END AS national_holiday
 from {{ref('prep_holidays_2019_us')}}
