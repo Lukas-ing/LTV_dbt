68e905f061bd18efca9260f800a96c8ddcf46e99
Lukas-ing <lukasgaethje@hotmail.de>
2025-07-31 14:48:04 +0200

    fixed bug with the classification

--- models/mart/mart_holidays_2019_us.sql
+++ models/mart/mart_holidays_2019_us.sql
@@ -1,14 +1,23 @@
 SELECT h.*,
 CASE 
-	WHEN lower(h.type) IN ('christian','hebrew','hinduism','muslim','local holiday, christian','national holiday, christian') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',1)) IN ('christian','hebrew','hinduism','muslim') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',2)) IN ('christian','hebrew','hinduism','muslim') THEN TRUE 
 	ELSE FALSE 
 END AS religious_holiday,
 CASE 
-	WHEN lower(h.type) IN ('local holiday','local holiday, christian','national holiday, christian','national holiday') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',1)) IN ('local holiday') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',2)) IN ('local holiday') THEN TRUE 
 	ELSE FALSE 
 END AS local_holiday,
 CASE 
-	WHEN lower(h.type) IN ('national holiday','national holiday, christian') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',1)) IN ('national holiday') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',2)) IN ('national holiday') THEN TRUE 
 	ELSE FALSE 
-END AS national_holiday
+END AS national_holiday,
+CASE 
+	WHEN trim(split_part(lower(h.type),',',1)) IN ('worldwide observance') THEN TRUE 
+	WHEN trim(split_part(lower(h.type),',',2)) IN ('worldwide observance') THEN TRUE 
+	ELSE FALSE 
+END AS worldwide_observance
 from {{ref('prep_holidays_2019_us')}} h
+order by h.date asc
