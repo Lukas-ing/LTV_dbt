f50189f42b3133be8b19717a706b118e56b2d2cb
Lukas-ing <lukasgaethje@hotmail.de>
2025-08-01 13:25:08 +0200

    comma fixes

--- models/mart/mart_marketing_spend.sql
+++ models/mart/mart_marketing_spend.sql
@@ -2,7 +2,7 @@ WITH discounts_month AS (
 SELECT a.TRANSACTION_DATE AS date, 
 round(sum(a.AVG_PRICE*a.QUANTITY*a.DISCOUNT_PCT*0.01)::numeric,2) AS total_discounted_oavg,
 round(sum((a.AVG_PRICE*a.QUANTITY*a.DISCOUNT_PCT)/(100-a.DISCOUNT_PCT))::numeric,2) AS total_discounted_sum_adavg
-FROM MART_ALL_DATA a
+FROM {{ref('mart_all_data')}}a
 WHERE a.COUPON_STATUS = 'Used' --OR a.COUPON_STATUS = 'Clicked'
 GROUP BY "date" 
 ORDER BY "date")
