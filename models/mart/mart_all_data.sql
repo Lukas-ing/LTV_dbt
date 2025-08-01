ffa0e624de78109cbf1e6ed5de20ed31cc611eca
Lukas-ing <lukasgaethje@hotmail.de>
2025-08-01 13:21:22 +0200

    renaming of colums

--- models/mart/mart_all_data.sql
+++ models/mart/mart_all_data.sql
@@ -1,9 +1,9 @@
-SELECT o.CUSTOMER_ID,
+SELECT o.TRANSACTION_DATE, 
+o.TRANSACTION_ID, 
+o.CUSTOMER_ID,
 c.GENDER  AS customer_gender,
 c."location" AS customer_location,
 c.TENURE_MONTHS AS customer_tenure_months,
-o.TRANSACTION_ID, 
-o.TRANSACTION_DATE, 
 o.PRODUKT_SKU,
 o.PRODUCT_CATEGORY,
 o.QUANTITY,
@@ -13,13 +13,13 @@ o.revenue,
 o.COUPON_STATUS,
 d.discount_pct,
 t.gst AS gst_onsale,
-m.OFFLINE_SPEND AS offline_spend_that_day, 
-m.ONLINE_SPEND AS online_spend_that_day,
-o.TRANSACTION_YEAR,
+m.OFFLINE_SPEND AS daily_offline, 
+m.ONLINE_SPEND AS daily_online,
+--o.TRANSACTION_YEAR,
 o.TRANSACTION_MONTH,
 o.TRANSACTION_MONTH_STR,
-o.TRANSACTION_DAY_OF_MONTH,
-o.TRANSACTION_DAY_OF_WEEK
+--o.TRANSACTION_DAY_OF_MONTH,
+--o.TRANSACTION_DAY_OF_WEEK
 FROM {{ref('prep_online_sales')}} o
 LEFT JOIN {{ref('prep_customers')}} c USING (customer_id)
 LEFT JOIN {{ref('prep_tax_amount')}} t USING (PRODUCT_CATEGORY)
--- models/mart/mart_marketing_spend.sql
+++ models/mart/mart_marketing_spend.sql
@@ -1,2 +1,10 @@
+WITH discounts_month AS (
+SELECT a.TRANSACTION_DATE AS date, 
+round(sum(a.AVG_PRICE*a.QUANTITY*a.DISCOUNT_PCT*0.01)::numeric,2) AS total_discounted_oavg,
+round(sum((a.AVG_PRICE*a.QUANTITY*a.DISCOUNT_PCT)/(100-a.DISCOUNT_PCT))::numeric,2) AS total_discounted_sum_adavg
+FROM MART_ALL_DATA a
+WHERE a.COUPON_STATUS = 'Used' --OR a.COUPON_STATUS = 'Clicked'
+GROUP BY "date" 
+ORDER BY "date")
 Select *
 from {{ref('prep_marketing_spend')}}
